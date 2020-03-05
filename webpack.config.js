8f0ae93616244cf25e1e495b98e1344dfacd8d21
fk00 <ser96876002@yandex.ru>
2020-03-05 21:11:23 +0300

    1st dz

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: {
+		app: './index.jsx',
+	},
+	context: path.resolve(__dirname, "static_src"),
+	output: {
+		path: path.resolve(__dirname, "static", "build"),
+		filename: 'app.js',
+	},
+	module: {
+       rules: [
+           {
+               test: /\.(js|jsx)$/,
+               include: path.resolve(__dirname, "static_src"),
+               loader: 'babel-loader',
+               exclude: /node_modules/,
+               options: {
+                 presets: ['@babel/env', '@babel/react'],
+               }
+           },
+       ],
+   },
+	plugins: [
+		new HtmlWebpackPlugin({ template: path.resolve(__dirname, "static_src", "index.html")})
+	]
+};
